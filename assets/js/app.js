0acbf92021d18f87e944b5680745882b3dc3e4ed
AxelLlanderal <axel.llanderal@gmail.com>
2026-09-07 14:20:29 -0600

    Configurar URL del backend en Vercel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const API_URL =
-   "https://TU-PROYECTO.vercel.app/api/chat";
+   "https://1-2-aplicaciones-web-r2hsw7cd8-axelllanderal-2262s-projects.vercel.app/api/chat";
 
 const form = document.getElementById("chatForm");
 const input = document.getElementById("messageInput");
